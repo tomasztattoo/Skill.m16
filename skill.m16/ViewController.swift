c0a8209b9bff6bd6c8254de10cf641d0f393cebf
Artem Ziborov <ziart92@mail.ru>
2023-12-15 12:49:48 +0300

    Create Folder

---
+++ skill.m16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  skill.m16
+//
+//  Created by Артем Зиборов on 08.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
